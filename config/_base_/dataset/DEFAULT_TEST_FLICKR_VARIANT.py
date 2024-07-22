9a049ad6cb861b0e36629640d2828d40b3e39b5c
soumikg08 <112841704+soumikg08@users.noreply.github.com>
2024-07-22 18:22:54 -0400

    REC evaluation

--- config/_base_/dataset/DEFAULT_TEST_FLICKR_VARIANT.py
+++ config/_base_/dataset/DEFAULT_TEST_FLICKR_VARIANT.py
@@ -7,22 +7,22 @@ FLICKR_TEST_COMMON_CFG = dict(
 DEFAULT_TEST_FLICKR_VARIANT = dict(
     FLICKR_EVAL_with_box=dict(
         **FLICKR_TEST_COMMON_CFG,
-        filename=r'{{fileDirname}}/../../../data/CWB_flickr30k_eval.jsonl',
-        template_file=r'{{fileDirname}}/template/flickr30k.json',
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/CWB_flickr30k_eval.jsonl',
+        template_file='/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/flickr30k.json',
     ),
     FLICKR_EVAL_without_box=dict(
         **FLICKR_TEST_COMMON_CFG,
-        filename=r'{{fileDirname}}/../../../data/CWB_flickr30k_eval.jsonl',
-        template_file=r'{{fileDirname}}/template/image_cap.json',
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/CWB_flickr30k_eval.jsonl',
+        template_file='/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/image_cap.json',
     ),
     FLICKR_TEST_with_box=dict(
         **FLICKR_TEST_COMMON_CFG,
-        filename=r'{{fileDirname}}/../../../data/CWB_flickr30k_test.jsonl',
-        template_file=r'{{fileDirname}}/template/flickr30k.json',
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/CWB_flickr30k_test.jsonl',
+        template_file='/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/flickr30k.json',
     ),
     FLICKR_TEST_without_box=dict(
         **FLICKR_TEST_COMMON_CFG,
-        filename=r'{{fileDirname}}/../../../data/CWB_flickr30k_test.jsonl',
-        template_file=r'{{fileDirname}}/template/image_cap.json',
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/CWB_flickr30k_test.jsonl',
+        template_file='/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/templatee/image_cap.json',
     ),
 )
--- config/_base_/dataset/DEFAULT_TEST_GQA_VARIANT.py
+++ config/_base_/dataset/DEFAULT_TEST_GQA_VARIANT.py
@@ -1,6 +1,6 @@
 GQA_TEST_COMMON_CFG = dict(
     type='GQADataset',
-    image_folder=r'/data/public/multimodal/multimodal_data/gqa/images',
+    image_folder='/datasets/GQA/images',
     scene_graph_file=None,
     scene_graph_index=None,
 )
@@ -8,28 +8,28 @@ GQA_TEST_COMMON_CFG = dict(
 # use standard q-a mode
 DEFAULT_TEST_GQA_VARIANT = dict(
     GQA_Q_A_BALANCED=dict(
-        **GQA_TEST_COMMON_CFG, version="q-a", template_file=r"{{fileDirname}}/template/VQA.json",
-        filename=r'{{fileDirname}}/../../../data/gqa_testdev_balanced_questions.jsonl'
+        **GQA_TEST_COMMON_CFG, version="q-a", template_file="/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/VQA.json",
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/gqa_testdev_balanced_questions.jsonl'
     ),
     GQA_Q_C_BALANCED=dict(
-        **GQA_TEST_COMMON_CFG, version="q-a", template_file=r"{{fileDirname}}/template/VQA_CoT.json",
-        filename=r'{{fileDirname}}/../../../data/gqa_testdev_balanced_questions.jsonl'
+        **GQA_TEST_COMMON_CFG, version="q-a", template_file="/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/VQA_CoT.json",
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/gqa_testdev_balanced_questions.jsonl'
     ),
     GQA_Q_BC_BALANCED=dict(
-        **GQA_TEST_COMMON_CFG, version="q-a", template_file=r"{{fileDirname}}/template/VQA_BCoT.json",
-        filename=r'{{fileDirname}}/../../../data/gqa_testdev_balanced_questions.jsonl'
+        **GQA_TEST_COMMON_CFG, version="q-a", template_file="/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/VQA_BCoT.json",
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/gqa_testdev_balanced_questions.jsonl'
     ),
 
     GQA_Q_A=dict(
-        **GQA_TEST_COMMON_CFG, version="q-a", template_file=r"{{fileDirname}}/template/VQA.json",
-        filename=r'{{fileDirname}}/../../../data/gqa_testdev_all_questions.jsonl',
+        **GQA_TEST_COMMON_CFG, version="q-a", template_file="/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/VQA.json",
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/gqa_testdev_all_questions.jsonl',
     ),
     GQA_Q_C=dict(
-        **GQA_TEST_COMMON_CFG, version="q-a", template_file=r"{{fileDirname}}/template/VQA_CoT.json",
-        filename=r'{{fileDirname}}/../../../data/gqa_testdev_all_questions.jsonl',
+        **GQA_TEST_COMMON_CFG, version="q-a", template_file="/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/VQA_CoT.json",
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/gqa_testdev_all_questions.jsonl',
     ),
     GQA_Q_BC=dict(
-        **GQA_TEST_COMMON_CFG, version="q-a", template_file=r"{{fileDirname}}/template/VQA_BCoT.json",
-        filename=r'{{fileDirname}}/../../../data/gqa_testdev_all_questions.jsonl',
+        **GQA_TEST_COMMON_CFG, version="q-a", template_file="/lustre/fs1/home/sghosh2/NExT-Chat/config/_base_/dataset/template/VQA_BCoT.json",
+        filename='/lustre/fs1/home/sghosh2/NExT-Chat/additional/gqa_testdev_all_questions.jsonl',
     ),
 )
